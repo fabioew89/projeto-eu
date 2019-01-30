6128fbea3c0575c4272689b830f3cfd8ff1d0c49
Fábio Ewerton <fabioew89@gmail.com>
2019-01-30 18:12:40 -0400

    site 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,35 @@
-var gulp = require('gulp');
-var html = require('gulp-htmlmin');
-var sass = require('gulp-sass');
-var notify = require('gulp-notify');
-var browserSync = require('browser-sync').create();
+const  {series, src, dest, watch} = require('gulp');
+const html = require('gulp-htmlmin');
+const sass = require('gulp-sass');
+const notify = require('gulp-notify');
+const browserSync = require('browser-sync').create();
 
-// html
-gulp.task('html', function() {
-    gulp.src('src/index.html')
-      .pipe(html({collapseWhitespace:true}))
-      .on("error", notify.onError("Error: <%= error.message %>"))
-      .pipe(gulp.dest('dist/'))
-      .pipe(browserSync.stream())
-});
+function clearHtml(){
+  return src('./src/index.html')
+        .pipe(html({collapseWhitespace:true}))
+        .on("error", notify.onError("Error: <%= error.message %>"))
+        .pipe(dest('./dist/'))
+        .pipe(browserSync.stream())
+}
 
-// sass
-gulp.task('sass', function() {
-    gulp.src('src/scss/style.scss')
-      .pipe(sass({outputStyle:"compressed"}))
-      .on("error", notify.onError("Error: <%= error.message %>"))
-      .pipe(gulp.dest('dist/css'))
-      .pipe(browserSync.stream())
-});
+function clearSass(){
+  return src('./src/scss/style.scss')
+        .pipe(sass({outputStyle:'collapsed'}))
+        .on("error", notify.onError("Error: <%= error.message %>"))
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream())
 
-// img
-gulp.task('moveImg', function() {
-    gulp.src('src/img/**/*')
-      .pipe(gulp.dest('dist/img/'));
-});
+}
 
-// Static server
-gulp.task('bs',['html','sass','moveImg'], function() {
-    browserSync.init({
-        server: {
-            baseDir: "dist/"
-        }
-    });
+function BS(){
+  browserSync.init({
+    server:{
+      baseDir:'./dist/'
+    }
+  })
 
-    gulp.watch('src/index.html', ['html']);
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-});
+  watch('./src/index.html', clearHtml)
+  watch('./src/scss/style.scss', clearSass)
+}
 
-// default
-gulp.task('default',['bs']);
+exports.default = series(BS, clearHtml, clearSass)
